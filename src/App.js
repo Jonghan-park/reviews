6fe7dd242b29b0c1a4a9f1949ce877f92a59b7ca
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-22 22:07:23 -0600

    Update app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Reviews from './Reviews';
 
 function App() {
-  return <h2>
-    reviews project setup
-    </h2>;
+  return (
+  <main>
+    <section className='container'>
+      <div className='title'>
+        <h2>our reviews</h2>
+          <div className='underline'></div>
+      </div>
+      <Reviews />
+    </section>
+  </main>
+  );
 }
 
 export default App;
